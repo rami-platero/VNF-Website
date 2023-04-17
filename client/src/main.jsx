4be93ea180d49fc7a63ab3d10cbbd544adb0ae3d
Inzanic <ramiplatero11@gmail.com>
2023-04-17 03:15:48 -0300

    fjikwejf

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./Application";
 import "./index.css";
 import { AuthContextProvider } from "./context/authContext";
-import { SongsContext } from "./context/SongsContext";
+import { SongsContext } from "./context/SongsContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthContextProvider>
